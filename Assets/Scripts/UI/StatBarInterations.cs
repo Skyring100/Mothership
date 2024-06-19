3688930aaff90871a1651ef2419377fc7883f68f
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-06-18 23:37:50 -0700

    Background of UI is now a darker color of the stat color rather than just black

--- Assets/Scripts/UI/StatBarInterations.cs
+++ Assets/Scripts/UI/StatBarInterations.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class StatBarInterations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{
+public class StatBarInterations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
     private StatsUIController crtler; 
     private Color color;
+    private static float backgroundModif = 0.2f;
     private void Start() {
         crtler = GameObject.Find("Player Stats UI").GetComponent<StatsUIController>();
         color = transform.GetChild(0).GetComponent<Image>().color;
+        //set the background color to be a darkened version of the stat color
+        GetComponent<Image>().color = new Color(color.r*backgroundModif, color.g*backgroundModif, color.b*backgroundModif);
     }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+    public void OnPointerEnter(PointerEventData eventData){
         crtler.ShowStatText(gameObject.name, color);
     }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
+    public void OnPointerExit(PointerEventData eventData){
         crtler.HideStatText();
     }
 }
