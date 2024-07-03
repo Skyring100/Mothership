6772bf62bed12620e461c797e0ed8c4b963cbe05
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2024-07-02 18:24:11 -0700

    Revert "Camera zoom is now a toggle button rather than hold down"
    This reverts commit 802e73ec2c6d322ea3f3b6518a45a3555e8e736d.

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -8,40 +8,35 @@ public class CameraController : MonoBehaviour
 
     private Camera cam;
     private Transform player;
-    [SerializeField] private float zoomRate;
+    [SerializeField] private float zoomOutRate;
     [SerializeField] private float maxZoomOut;
+    [SerializeField] private float zoomInRate;
     [SerializeField] private float baseZoom;
     [SerializeField] private SpriteRenderer background;
     private Vector3 lastPos;
     [SerializeField] private float backgroundSpeed;
-    [SerializeField] private bool isZoomedOut;
     
     private void Start(){
         player = GameObject.FindGameObjectWithTag("Player").transform;
         cam = GetComponent<Camera>();
         lastPos = transform.position;
-        isZoomedOut = false;
-        cam.orthographicSize = baseZoom;
     }
-    private void Update(){
+    private void FixedUpdate(){
         if(!player.IsDestroyed()){
             transform.position = new Vector3(player.position.x, player.position.y, -10);
         }
-        if(Input.GetKeyDown(KeyCode.Space)){
-            isZoomedOut = !isZoomedOut;
-        }
-        if(isZoomedOut){
+        if(Input.GetKey(KeyCode.Space)){
             //zoom out camera
             if(cam.orthographicSize < maxZoomOut){
-                cam.orthographicSize += zoomRate * Time.deltaTime;
+                cam.orthographicSize += zoomOutRate;
                 if(cam.orthographicSize > maxZoomOut){
                     cam.orthographicSize = maxZoomOut;
                 }
             }
         }else{
-            //return to normal zoom
+            //if no longer pressing, return to normal zoom
             if(cam.orthographicSize != baseZoom){
-                cam.orthographicSize -= zoomRate * Time.deltaTime;
+                cam.orthographicSize -= zoomInRate;
                 if(cam.orthographicSize < baseZoom){
                     cam.orthographicSize = baseZoom;
                 }
@@ -50,9 +45,8 @@ public class CameraController : MonoBehaviour
         //moving the background with the player
         if(!transform.position.Equals(lastPos)){
             Vector2 dir = transform.position - lastPos;
-            background.material.mainTextureOffset += backgroundSpeed * Time.deltaTime * dir; 
+            background.material.mainTextureOffset += dir * Time.deltaTime * backgroundSpeed; 
         }
         lastPos = transform.position;
     }
-
 }
